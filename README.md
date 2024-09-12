a2d112df666f7db83c5fb436c16c43e2b4efa190
Caitlyn Wong <91009885+caitlynkywong@users.noreply.github.com>
2024-09-12 23:16:26 +1000

    Upload week01 code

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# 24T3-COMP1511-W16A
+# 24T3 COMP1511 W16A
+
+This is the GitHub for our Wednesday 4pm tutorials! Click on one of the folders above to view the tutorial code for that week :)
+
+Notion (tutorial notes): https://caitlynkywong.notion.site/24T3-COMP1511-W16A-cff3dcae4fcb4da8ae462eba8eab14f7
---
+++ week01/face1.c
@@ -0,0 +1,23 @@
+// face1.c
+//
+// Written by W16A,
+// on 11/09/2024
+//
+// The program prints one simple face
+//
+// Face 1:
+//  ~ ~
+//  0 0
+//   o
+//   -
+
+#include <stdio.h>
+
+int main(void) {
+    printf("~ ~\n");
+    printf("0 0\n");
+    printf(" o \n");
+    printf(" - \n");
+    
+    return 0;
+}
